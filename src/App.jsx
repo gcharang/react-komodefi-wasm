8248dc3587d1c8ddd008223966bc15d04321a9f6
gcharang <21151592+gcharang@users.noreply.github.com>
2022-12-23 01:05:56 +0530

    mm2 launched and output seen

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@
 // will "boot" the module and make it ready to use.
 // Currently, browsers don't support natively imported WebAssembly as an ES module, but
 // eventually the manual initialization won't be required!
-import {useEffect, useRef, useState} from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import init, {
   mm2_main,
@@ -26,13 +26,27 @@ const LOG_LEVEL = LogLevel.Debug;
 // wasm to be loaded.
 async function init_wasm() {
   try {
-    await init();
+    const baseUrl = getBaseUrl()
+    let mm2BinUrl = new URL(baseUrl + "/mm2_bg.wasm")
+    await init(mm2BinUrl);
   } catch (e) {
     alert(`Oops: ${e}`);
   }
 }
 
-async function run_mm2(params) {
+// async function run_mm2_on_click() {
+//   try {
+//     const response = await mm2_rpc(request_js);
+//     console.log(response);
+//     const conf = document.getElementById("wid_conf_input").value || document.getElementById("wid_conf_input").defaultValue;
+//         console.log(conf)
+//   } catch (e) {
+//     console.log(e)
+//   }
+// }
+
+
+async function run_mm2(params, handle_log) {
   // run an MM2 instance
   try {
     const version = mm2_version();
@@ -79,28 +93,18 @@ async function rpc_request(request_js) {
   }
 }
 
-function handle_log(level, line) {
-  switch (level) {
-    case LogLevel.Off:
+const getBaseUrl = () => {
+  let url;
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      url = 'https://atomicdex-play.lordofthechains.com';
       break;
-    case LogLevel.Error:
-      console.error(line);
-      break;
-    case LogLevel.Warn:
-      console.warn(line);
-      break;
-    case LogLevel.Info:
-      console.info(line);
-      break;
-    case LogLevel.Debug:
-      console.log(line);
-      break;
-    case LogLevel.Trace:
+    case 'development':
     default:
-      // The console.trace method outputs some extra trace from the generated JS glue code which we don't want.
-      console.debug(line);
-      break;
+      url = 'http://localhost:1234';
   }
+
+  return url;
 }
 
 function spawn_mm2_status_checking() {
@@ -111,12 +115,16 @@ function spawn_mm2_status_checking() {
     const status = mm2_main_status();
     switch (status) {
       case MainStatus.NotRunning:
+      //  console.log("NotRunning")
       case MainStatus.NoContext:
+      // console.log("NoContext")
       case MainStatus.NoRpc:
+      //  console.log("NoRpc")
         rpc_button.disabled = true;
         run_button.disabled = false;
         break;
       case MainStatus.RpcIsUp:
+      //  console.log("RpcIsUp")
         rpc_button.disabled = false;
         run_button.disabled = true;
         break;
@@ -130,29 +138,130 @@ function spawn_mm2_status_checking() {
 function App() {
   const outputBottomRef = useRef(null);
 
-  const [outputMessages, setOutputMessages] = useState([]);
+  const [outputMessages, setOutputMessages] = useState([["Once mm2 is run, daemon output is rendered here", "blue"]]);
 
-  useEffect(() => {
-    // 👇️ simulate chat outputMessages flowing in
-    setInterval(
-      () =>
-        setOutputMessages(current => [
-          ...current,
-          'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, quaerat eum id obcaecati, magnam voluptatum dolorem sunt, omnis sed consectetur necessitatibus blanditiis ipsa? Cumque architecto, doloribus mollitia velit non sint!'+Date.now(),
-        ]),
-      600,
-    );
-  }, []);
+  /*useEffect(() => {
+   // 👇️ simulate chat Messages flowing in
+   setInterval(
+     () =>
+       setOutputMessages(current => [
+         ...current,
+         ['Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, quaerat eum id obcaecati, magnam voluptatum dolorem sunt, omnis sed consectetur necessitatibus blanditiis ipsa? Cumque architecto, doloribus mollitia velit non sint!'+Date.now(),"blue"],
+       ]),
+     600,
+   );
+ }, []);*/
 
   useEffect(() => {
     // 👇️ scroll to bottom every time outputMessages change
     let nOfOutputMsgs = outputMessages.length
-    if (nOfOutputMsgs > 20) {
+    if (nOfOutputMsgs > 500) {
+      setOutputMessages(current => current.slice(-500))
       //outputMessages = outputMessages.slice(-20)
-      outputMessages.splice(0,nOfOutputMsgs-20)
+      // outputMessages.splice(0,nOfOutputMsgs-20)
     }
-    outputBottomRef.current?.scrollIntoView({behavior: 'smooth'});
+    outputBottomRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [outputMessages]);
+
+  function handle_log(level, line) {
+    switch (level) {
+      case LogLevel.Off:
+        break;
+      case LogLevel.Error:
+        setOutputMessages(current => [
+          ...current,
+          ["[Error] "+line, "red"],
+        ]);
+        console.error(line);
+        break;
+      case LogLevel.Warn:
+        setOutputMessages(current => [
+          ...current,
+          ["[Warn] "+line, "yellow"],
+        ]);
+        console.warn(line);
+        break;
+      case LogLevel.Info:
+        setOutputMessages(current => [
+          ...current,
+          ["[Info] "+line, "violet"],
+        ]);
+        console.info(line);
+        break;
+      case LogLevel.Debug:
+        // setOutputMessages(current => [
+        //   ...current,
+        //   ["[Debug] "+line, "neutral"],
+        // ]);
+        console.log(line);
+        break;
+      case LogLevel.Trace:
+      default:
+        // The console.trace method outputs some extra trace from the generated JS glue code which we don't want.
+        setOutputMessages(current => [
+          ...current,
+          ["[default] "+line, "neutral"],
+        ]);
+        console.debug(line);
+        break;
+    }
+  }
+
+  useEffect(() => {
+    /* (async () => {
+       await init_wasm()
+       spawn_mm2_status_checking();
+     })();
+ 
+     return () => {
+       // this now gets called when the component unmounts
+     };*/
+
+    init_wasm().then(function () {
+      spawn_mm2_status_checking();
+      const run_mm2_button = document.getElementById("wid_run_mm2_button");
+      run_mm2_button.addEventListener('click', async () => {
+        const conf = document.getElementById("wid_conf_input").value || document.getElementById("wid_conf_input").defaultValue;
+
+        let params;
+        try {
+          const conf_js = JSON.parse(conf);
+          const baseUrl = getBaseUrl()
+          let coinsUrl = new URL(baseUrl + "/coins")
+          let coins = await fetch(coinsUrl);
+          let coinsJson = await coins.json();
+          // console.log(JSON.stringify(conf_js))
+          conf_js.coins = coinsJson
+          console.log(conf_js)
+          params = {
+            conf: conf_js,
+            log_level: LOG_LEVEL,
+          };
+        } catch (e) {
+          alert(`Expected config in JSON, found '${conf}'\nError : ${e}`);
+          return;
+        }
+
+        await run_mm2(params, handle_log);
+      });
+
+      const rpc_request_button = document.getElementById("wid_mm2_rpc_button");
+      rpc_request_button.addEventListener('click', async () => {
+        const request_payload = document.getElementById("wid_rpc_input").value;
+        let request_js;
+        try {
+          request_js = JSON.parse(request_payload);
+        } catch (e) {
+          alert(`Expected request in JSON, found '${request_payload}'\nError : ${e}`);
+          return;
+        }
+
+        await rpc_request(request_js);
+      });
+    });
+
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
 
   return (
     <div>
@@ -163,25 +272,25 @@ function App() {
             {/* Left column */}
             <div className="grid h-full  grid-cols-1 gap-4 lg:col-span-2">
               <section aria-labelledby="section-1-title" className="flex flex-col justify-between">
-                <textarea id="wid_conf_input" className="w-full h-[30vh] rounded-lg bg-slate-800 shadow text-gray-300 p-2">{`{
+                <textarea id="wid_conf_input" className="w-full h-[30vh] rounded-lg bg-slate-800 shadow text-gray-300 p-2" defaultValue={`{
     "gui": "WASMTEST",
     "mm2": 1,
     "passphrase": "wasmtest",
     "allow_weak_password": true,
     "rpc_password": "testpsw",
     "netid": 7777
-}`}
+}`}>
                 </textarea>
 
-                <button id="wid_run_mm2_button" disabled="disabled" className="inline-flex justify-center rounded-lg text-sm font-semibold  px-4 my-2 bg-slate-300 text-gray-500 hover:bg-slate-100 h-[32px] w-[142px] mx-auto">
+                <button id="wid_run_mm2_button"  className="inline-flex justify-center rounded-lg text-sm font-semibold  px-4 my-2 bg-slate-300 text-gray-500 hover:bg-slate-100 h-[32px] w-[142px] mx-auto">
                   <span className="my-auto flex items-center">Run mm2</span>
                 </button>
-                <div readOnly="readonly" id="wid_mm2_output" className="w-full h-[60vh] overflow-y-scroll rounded-lg bg-slate-800 shadow text-gray-300 p-4">
-                {outputMessages.map((message, index) => {
-          return <div className="text-base font-bold border-slate-700	border-b-2" key={index}><p className="">{message}</p></div>;
-        })}
-                    {/* <p className="text-xl font-bold">Once mm2 is run, daemon output is rendered here</p>               */}
-                    <div ref={outputBottomRef} />
+                <div id="wid_mm2_output" className="w-full h-[60vh] overflow-y-scroll rounded-lg bg-slate-800 shadow text-gray-300 p-4">
+                  {outputMessages.map((message, index) => {
+                    return <div className="text-base font-bold border-slate-700	border-b-2" key={index}><p className={`text-${message[1]}-300`}>{message[0]}</p></div>;
+                  })}
+                  {/* <p className="text-xl font-bold">Once mm2 is run, daemon output is rendered here</p>               */}
+                  <div ref={outputBottomRef} className="text-blue-300 text-violet-300 text-red-300 text-yellow-300 text-nuetral-300" />
                 </div>
               </section>
             </div>
@@ -190,7 +299,7 @@ function App() {
             <div className="grid h-full grid-cols-1 gap-4">
               <section aria-labelledby="section-2-title" className="flex flex-col justify-between">
 
-                <textarea id="wid_rpc_input" className="w-full h-[30vh] rounded-lg bg-slate-800 shadow text-gray-300 p-2" >{`[
+                <textarea id="wid_rpc_input" className="w-full h-[30vh] rounded-lg bg-slate-800 shadow text-gray-300 p-2" defaultValue={`[
     {
         "userpass": "testpsw",
         "method": "electrum",
@@ -227,12 +336,11 @@ function App() {
             "http://eth1.cipig.net:8555"
         ]
     }
-]`}
-                </textarea>
-                <button id="wid_mm2_rpc_button" disabled="disabled" className="inline-flex justify-center rounded-lg text-sm font-semibold my-2 px-4 bg-slate-300 text-gray-500 hover:bg-slate-100  h-[32px] w-[142px] mx-auto">
+]`}></textarea>
+                <button id="wid_mm2_rpc_button" className="inline-flex justify-center rounded-lg text-sm font-semibold my-2 px-4 bg-slate-300 text-gray-500 hover:bg-slate-100  h-[32px] w-[142px] mx-auto">
                   <span className="my-auto flex items-center">Send request</span>
                 </button>
-                <textarea readOnly="readonly" id="wid_rpc_output" className="w-full h-[60vh] rounded-lg bg-slate-800 shadow text-gray-300 p-4">Once a request is sent, mm2's response is displayed here
+                <textarea readOnly="readonly" id="wid_rpc_output" className="w-full h-[60vh] rounded-lg bg-slate-800 shadow text-gray-300 p-4" defaultValue="Once a request is sent, mm2's response is displayed here">
                 </textarea>
               </section>
             </div>
