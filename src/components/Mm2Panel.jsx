ce8a2af264e9c9a1c954f9958fd507a5a0906cdf
emmaccen <oriolaemmanuel199@gmail.com>
2024-04-30 18:14:04 +0100

    Removed iFrame from layout

--- src/components/Mm2Panel.jsx
+++ src/components/Mm2Panel.jsx
@@ -291,11 +291,11 @@ const Mm2Panel = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <iframe
+      {/* <iframe
         id="sandbox"
         src={`${getBaseUrl()}/bridge`}
         className="w-0 h-0"
-      ></iframe>
+      ></iframe> */}
       <div className="w-full p-2 bg-primaryLight text-[#a2a3bd] h-10 border-b border-b-gray-800">
         <div className="flex justify-between">
           <div className="flex gap-3">
