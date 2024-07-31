5d8a84b955e81bc5df774d904994554a83a3e93d
smk762 <smk@komodoplatform.com>
2024-07-31 22:46:26 +0800

    tweak update_wasm script to work on macos also

--- update_wasm.sh
+++ update_wasm.sh
@@ -8,7 +8,11 @@ mkdir temp
 unzip $fn -d temp
 cd temp
 mv kdflib_bg.wasm ../../public/kdflib_bg.wasm
-sed -i "s|input = new URL('kdflib_bg.wasm', import.meta.url);|input = new URL('kdflib_bg.wasm', process.env.NEXT_PUBLIC_BASE_PATH);|" kdflib.js
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s|input = new URL('kdflib_bg.wasm', import.meta.url);|input = new URL('kdflib_bg.wasm', process.env.NEXT_PUBLIC_BASE_PATH);|" kdflib.js
+else
+    sed -i "s|input = new URL('kdflib_bg.wasm', import.meta.url);|input = new URL('kdflib_bg.wasm', process.env.NEXT_PUBLIC_BASE_PATH);|" kdflib.js
+fi
 
 mv kdflib.js ../../src/js/kdflib.js
 cp -r snippets/* ../../src/js/snippets/
